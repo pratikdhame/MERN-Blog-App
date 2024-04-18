dfeba4d80f1bdef5f973bb0138672785621b684e
Pratik Dhame <96607747+pratiktechie@users.noreply.github.com>
2024-04-18 17:51:05 +0530

    completed till 1hr 37min ( Post Details/Single Post Page

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,47 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 
-import Thumbnail1 from '../images/blog1.jpg'
-import Thumbnail2 from '../images/blog2.jpg'
-import Thumbnail3 from '../images/blog3.jpg'
-import Thumbnail4 from '../images/blog4.jpg'
-
 import PostItem from './PostItem'
-
-const DUMMY_POSTS =[
-    {
-        id: '1',
-        thumbnail: Thumbnail1,
-        category: 'education',
-        title: 'This it the title of the very first post on this blog',
-        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
-        authorID: 3
-    },
-    {
-        id: '2',
-        thumbnail: Thumbnail2,
-        category: 'education',
-        title: 'This it the title of the very first post on this blog',
-        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
-        authorID: 3
-    },
-    {
-        id: '3',
-        thumbnail: Thumbnail3,
-        category: 'education',
-        title: 'This it the title of the very first post on this blog',
-        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
-        authorID: 3
-    },
-    {
-        id: '4',
-        thumbnail: Thumbnail4,
-        category: 'education',
-        title: 'This it the title of the very first post on this blog',
-        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
-        authorID: 3
-    }
-]
+import { DUMMY_POSTS } from '../data'
 
 const Posts = () => {
 
@@ -49,12 +10,12 @@ const Posts = () => {
 
   return (
     <section className="posts">
-        <div className="container posts__container">
+    {posts.length > 0 ? <div className="container posts__container">
         {
             posts.map(({id, thumbnail, category, title, desc, authorID}) => 
             <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={authorID}/>)
         }
-        </div>
+        </div>: <h2 className='center'>No Posts Found</h2>}
     </section>
   )
 }
---
+++ client/src/data.js
@@ -0,0 +1,39 @@
+import Thumbnail1 from './images/blog1.jpg'
+import Thumbnail2 from './images/blog2.jpg'
+import Thumbnail3 from './images/blog3.jpg'
+import Thumbnail4 from './images/blog4.jpg'
+
+export const DUMMY_POSTS =[
+    {
+        id: '1',
+        thumbnail: Thumbnail1,
+        category: 'education',
+        title: 'This it the title of the very first post on this blog',
+        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
+        authorID: 3
+    },
+    {
+        id: '2',
+        thumbnail: Thumbnail2,
+        category: 'education',
+        title: 'This it the title of the very first post on this blog',
+        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
+        authorID: 3
+    },
+    {
+        id: '3',
+        thumbnail: Thumbnail3,
+        category: 'education',
+        title: 'This it the title of the very first post on this blog',
+        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
+        authorID: 3
+    },
+    {
+        id: '4',
+        thumbnail: Thumbnail4,
+        category: 'education',
+        title: 'This it the title of the very first post on this blog',
+        desc: 'ipsum dolor sit amet consectetur adipisicing elit. Sunt mollitia deleniti corporis fuga reiciendis at, consequatur rem assumenda molestias, quo rerum autem ducimus aliquid dolore perferendis ullam dignissimos earum neque quibusdam tempore provident velit excepturi nihil.',
+        authorID: 3
+    }
+]
