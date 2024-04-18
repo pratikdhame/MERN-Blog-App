43e31e408e113e8d65551129bab7e736bf581c3c
Pratik Dhame <96607747+pratiktechie@users.noreply.github.com>
2024-04-18 15:58:09 +0530

    done with the basic styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import Header from './Header'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <footer>
+        <ul className="footer__categories">
+          <li><Link to="/posts/categories/Agriculture">Agriculture</Link></li>
+          <li><Link to="/posts/categories/Business">Business</Link></li>
+          <li><Link to="/posts/categories/Education">Education</Link></li>
+          <li><Link to="/posts/categories/Entertainment">Entertainment</Link></li>
+          <li><Link to="/posts/categories/Art">Art</Link></li>
+          <li><Link to="/posts/categories/Investment">Investment</Link></li>
+          <li><Link to="/posts/categories/Uncategorized">Uncategorized</Link></li>
+          <li><Link to="/posts/categories/Agriculture">Weather</Link></li>
+        </ul>
+        <div className="footer__copyright">
+          <small> All Rights Reserved &copy; Copyright, Pratik Dhame</small>
+        </div>
+
+    </footer>
   )
 }
 
